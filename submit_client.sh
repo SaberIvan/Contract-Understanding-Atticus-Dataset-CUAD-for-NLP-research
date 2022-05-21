e33f7e83677a011e2b9358587710584ab0e1b53c
SaberIvan <37329617+SaberIvan@users.noreply.github.com>
2022-05-21 15:21:06 +0800

    Add files via upload

---
+++ submit_client.sh
@@ -0,0 +1,5 @@
+spark-submit \
+    --master yarn \
+    --deploy-mode client \
+    Ass2.py \
+    --output $1
---
+++ submit_cluster.sh
@@ -0,0 +1,5 @@
+spark-submit \
+    --master yarn \
+    --deploy-mode cluster \
+    Ass2.py \
+    --output $1
